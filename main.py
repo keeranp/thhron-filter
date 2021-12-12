6ce30df0267c68c44b600a08e967d95686f1c4ea
keeranp <keeran.paheerathan@gmail.com>
2021-12-12 12:13:14 +0100

    Added CORS allowed origins

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ import cv2
 import numpy as np
 import io
 from starlette.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from gen import generate
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def root():
