82ee4c4f83bfdc72f5125cfabc10f5d103ad0283
keeranp <keeran.paheerathan@gmail.com>
2022-10-10 10:59:47 +0200

    Added color variation and triangle size scaling

--- gen.py
+++ gen.py
@@ -2,27 +2,36 @@ import cv2
 import numpy as np
 from random import randrange, randint
 
+
 def draw_triangle(r_image, f_image):
     ''' Draws a triangle randomly in the fake image around a random point selected in the real image'''
 
-    TRIANGLE_SIZE = 15
-
     img_shape = r_image.shape
 
-    #Get a random point from the real image
+    TRIANGLE_SIZE = int(img_shape[0] * img_shape[1] / 20000)
+    COLOR_VARIATION = 50
+
+    # Get a random point from the real image
     pt = (randrange(img_shape[0]), randrange(img_shape[1]))
 
-    #Get the color of that point and convert it to int
-    color = (int(r_image[pt[0], pt[1], 0]),int(r_image[pt[0], pt[1], 1]),int(r_image[pt[0], pt[1], 2]))
+    # Get the color of that point and convert it to int
+    color = (int(r_image[pt[0], pt[1], 0]), int(
+        r_image[pt[0], pt[1], 1]), int(r_image[pt[0], pt[1], 2]))
+    color = (color[0] + randint(-COLOR_VARIATION, COLOR_VARIATION), color[1] +
+             randint(-COLOR_VARIATION, COLOR_VARIATION), color[2] + randint(-COLOR_VARIATION, COLOR_VARIATION))
 
-    pt1 = (pt[1] + randint(-TRIANGLE_SIZE, TRIANGLE_SIZE), pt[0] + randint(-TRIANGLE_SIZE, TRIANGLE_SIZE))
-    pt2 = (pt[1] + randint(-TRIANGLE_SIZE, TRIANGLE_SIZE), pt[0] + randint(-TRIANGLE_SIZE, TRIANGLE_SIZE))
-    pt3 = (pt[1] + randint(-TRIANGLE_SIZE, TRIANGLE_SIZE), pt[0] + randint(-TRIANGLE_SIZE, TRIANGLE_SIZE))
+    pt1 = (pt[1] + randint(-TRIANGLE_SIZE, TRIANGLE_SIZE),
+           pt[0] + randint(-TRIANGLE_SIZE, TRIANGLE_SIZE))
+    pt2 = (pt[1] + randint(-TRIANGLE_SIZE, TRIANGLE_SIZE),
+           pt[0] + randint(-TRIANGLE_SIZE, TRIANGLE_SIZE))
+    pt3 = (pt[1] + randint(-TRIANGLE_SIZE, TRIANGLE_SIZE),
+           pt[0] + randint(-TRIANGLE_SIZE, TRIANGLE_SIZE))
 
     triangle_pts = np.array([pt1, pt2, pt3])
 
-    #Draw triangle
-    cv2.drawContours(f_image,[triangle_pts], 0, color, -1)
+    # Draw triangle
+    cv2.drawContours(f_image, [triangle_pts], 0, color, -1)
+
 
 def generate(real_img):
     ''' Initialize images '''
